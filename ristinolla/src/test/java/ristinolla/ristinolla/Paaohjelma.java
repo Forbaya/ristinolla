b5833dd36d2223f0dee1e41012dab1744ea059e2
Ville Tanttu <vilunen@gmail.com>
2014-01-14 20:49:12 +0200

    konfiguroinnit tehty

--- ristinolla/src/test/java/ristinolla/ristinolla/Paaohjelma.java
+++ ristinolla/src/test/java/ristinolla/ristinolla/Paaohjelma.java
@@ -4,10 +4,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+public class Paaohjelma 
     extends TestCase
 {
     /**
@@ -15,7 +12,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public Paaohjelma( String testName )
     {
         super( testName );
     }
@@ -25,7 +22,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( Paaohjelma.class );
     }
 
     /**
