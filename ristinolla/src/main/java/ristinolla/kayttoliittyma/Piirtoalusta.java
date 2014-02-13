1ed1de828cb0aad4aff3adec004e027f38099e14
Ville Tanttu <vilunen@gmail.com>
2014-02-13 18:07:02 +0200

    Lisätty vaihtoehtoinen vaihtoehtoinen risti ja nolla

--- ristinolla/src/main/java/ristinolla/kayttoliittyma/Piirtoalusta.java
+++ ristinolla/src/main/java/ristinolla/kayttoliittyma/Piirtoalusta.java
@@ -33,24 +33,56 @@ public class Piirtoalusta extends JPanel {
         g.drawLine(0, 300, 300, 300);
     }
     
-    /** Piirtää X haluttuun koordinaattiin piirtoalustassa.
+    /** Piirtää normaalin ristin haluttuun koordinaattiin piirtoalustassa. Logiikan muuttujan ristinAsetus tulee olla 1,
+     *  jotta tällainen risti piirretään.
      * @param x Piirtokohdan x-koordinaatti
      * @param y Piirtokohdan y-koordinaatti
      */
-    public void piirraRisti(int x, int y) {
+    public void piirraNormaaliRisti(int x, int y) {
         Graphics graphics = this.getGraphics();
         graphics.setColor(Color.BLUE);
         graphics.drawLine(x, y, x + 50, y + 50);
         graphics.drawLine(x + 50, y, x, y + 50);
     }
     
-    /** Piirtää O haluttuun koordinaattiin piirtoalustassa.
+    /** Piirtää latinalaisen ristin haluttuun koordinaattiin piirtoalustassa. Logiikan muuttujan ristinAsetus tulee olla 2,
+     *  jotta tällainen risti piirretään.
      * @param x Piirtokohdan x-koordinaatti
      * @param y Piirtokohdan y-koordinaatti
      */
-    public void piirraNolla(int x, int y) {
+    public void piirraLatinalainenRisti(int x, int y) {
+        Graphics graphics = this.getGraphics();
+        graphics.setColor(Color.BLUE);
+        graphics.drawLine(x, y + 10, x + 50, y + 10);
+        graphics.drawLine(x + 25, y - 15, x + 25, y + 70);
+    }
+    
+    /** Piirtää normaalin nollan haluttuun koordinaattiin piirtoalustassa. Logiikan muuttujan nollanAsetus tulee olla 1,
+     *  jotta tällainen nolla piirretään.
+     * @param x Piirtokohdan x-koordinaatti
+     * @param y Piirtokohdan y-koordinaatti
+     */
+    public void piirraNormaaliNolla(int x, int y) {
         Graphics graphics = this.getGraphics();
         graphics.setColor(Color.RED);
         graphics.drawOval(x - 12, y - 12, 75, 75);
+    }
+    
+    /** Piirtää hymynaaman haluttuun koordinaattiin piirtoalustassa. Logiikan muuttujan nollanAsetus tulee olla 2,
+     *  jotta tällainen "nolla" piirretään.
+     * @param x Piirtokohdan x-koordinaatti
+     * @param y Piirtokohdan y-koordinaatti
+     */
+    public void piirraHymynaama(int x, int y) {
+        Graphics graphics = this.getGraphics();
+        graphics.setColor(Color.RED);
+        // Piirtää pään.
+        graphics.drawOval(x - 12, y - 12, 75, 75);
+        // Piirtää vasemman silmän.
+        graphics.drawOval(x + 10, y + 10, 10, 10);
+        // Piirtää oikean silmän.
+        graphics.drawOval(x + 30, y + 10, 10, 10);
+        // Piirtää suun.
+        graphics.drawArc(x, y + 15, 50, 30, 180, 180);
     }
 }
